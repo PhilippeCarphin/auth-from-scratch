78b1ecb559c30cd5a135955d27a3282011f983ca
Philippe Carphin <pcarphin@studio.local>
2025-01-11 15:13:54 -0500

    Hello world web app

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject clojure-web-app "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "This is me following a tutorial https://www.youtube.com/watch?v=Tq2t9uTTJj0"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring "1.8.0"]]
   :main ^:skip-aot clojure-web-app.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/clojure_web_app/core.clj
+++ src/clojure_web_app/core.clj
@@ -1,7 +1,20 @@
 (ns clojure-web-app.core
+  (:require [ring.adapter.jetty :refer [run-jetty]])
   (:gen-class))
 
+(defn app-handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "<H1>Hello World</H1>"})
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Main function of the application"
   [& args]
-  (println "Hello, World!"))
+  (run-jetty app-handler {:port 8080})
+)
+
+(defn hello []
+  (println "hello"))
+
+
+
